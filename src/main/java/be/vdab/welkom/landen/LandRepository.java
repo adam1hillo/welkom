d8541f192ff48046a08ef895bf64344a287ebf33
Adam Hillo <adam1hillo@gmail.com>
2024-03-25 13:22:01 +0100

    Bean

---
+++ src/main/java/be/vdab/welkom/landen/LandRepository.java
@@ -0,0 +1,10 @@
+package be.vdab.welkom.landen;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LandRepository {
+    public LandRepository() {
+        System.out.println("LandRepository constructor");
+    }
+}
