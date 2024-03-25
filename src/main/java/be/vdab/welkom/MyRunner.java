13613e81a1d2f6e059b6e20b0eb1894db5029d41
Adam Hillo <adam1hillo@gmail.com>
2024-03-25 15:37:03 +0100

    Dependency injection

--- src/main/java/be/vdab/welkom/MyRunner.java
+++ src/main/java/be/vdab/welkom/MyRunner.java
@@ -1,12 +1,25 @@
 package be.vdab.welkom;
 
+import be.vdab.welkom.landen.LandRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
+
+    private final LandRepository landRepository;
+
+    public MyRunner(LandRepository landRepository) {
+        this.landRepository = landRepository;
+    }
+
     @Override
     public void run(String... args) {
-        System.out.println("Hallo");
+        try {
+            landRepository.findAll()
+                    .forEach(land -> System.out.println(land.getNaam()));
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace(System.err);
+        }
     }
 }
