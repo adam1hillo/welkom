7cb9bf598a2b624d3c43680f67907d35d84aaf7a
Adam Hillo <adam1hillo@gmail.com>
2024-03-26 11:43:01 +0100

    application.properties

--- src/main/java/be/vdab/welkom/talen/XmlTaalRepository.java
+++ src/main/java/be/vdab/welkom/talen/XmlTaalRepository.java
@@ -2,7 +2,6 @@ package be.vdab.welkom.talen;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import javax.xml.stream.XMLInputFactory;
